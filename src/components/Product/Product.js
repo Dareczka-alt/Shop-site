4cf58d3fdee42f1b749d6562a204aaca8f46cbe8
Daria Dorna <perelka4@onet.pl>
2023-08-11 20:29:07 +0200

    Use useMemo hook

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,46 +1,46 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
+import ProductOptions from '../ProductOptions/ProductOptions';
+import ProductImage from '../ProductImage/ProductImage';
 
 const Product = props => {
   const [currentColor, setColor] = useState(`${props.colors[0]}`);
-  const [currentSize, setSize] = useState(`${props.sizes[0].names}`);
+  const [currentSize, setSize] = useState(`${props.sizes[0].name}`);
+
+
+  const getPrice = useMemo(() => {
+    for (let size of props.sizes) {
+      if (size.name === currentSize) {
+        return props.basePrice + size.additionalPrice
+      }
+    }
+  }, [currentSize, props.basePrice, props.sizes]);
+
+
+  const addToShoppingCart = (e) => {
+    e.preventDefault();
+    props.action({ id: props.id, title: props.title, size: currentSize, color: currentColor, price: getPrice })
+  }
 
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          alt={props.title}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
-      </div>
+      <ProductImage title={props.title} name={props.name} currentColor={currentColor} />
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {props.basePrice}$</span>
+          <span className={styles.price}>{`${getPrice}$`}</span>
         </header>
-        <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              {props.sizes.map(size => <li><button value={size.name} type="button" className={clsx(size.name === currentSize && styles.active)}>{size.name}</button></li>)}
-
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              {props.colors.map(color => <li><button type="button" className={clsx(styles[`color${color[0].toUpperCase()}${color.substr(1).toLowerCase()}`], color === currentColor && styles.active)}></button></li>)}
-
-            </ul>
-          </div>
-          <Button className={styles.button}>
-            <span className="fa fa-shopping-cart" />
-          </Button>
-        </form>
+        <ProductOptions
+          action={addToShoppingCart}
+          setSize={setSize}
+          setColor={setColor}
+          onSize={props.sizes}
+          onColor={props.colors}
+          currentColor={currentColor}
+          currentSize={currentSize}
+        />
       </div>
-    </article >
+    </article>
   )
 };
 
