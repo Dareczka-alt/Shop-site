6e30916fc91381337e714ec2023c289100b055c8
Daria Dorna <perelka4@onet.pl>
2023-08-11 19:31:09 +0200

    Finished site

---
+++ src/components/Product/Product.js
@@ -0,0 +1,47 @@
+import styles from './Product.module.scss';
+import clsx from 'clsx';
+import Button from '../Button/Button';
+import { useState } from 'react';
+
+const Product = props => {
+  const [currentColor, setColor] = useState(`${props.colors[0]}`);
+  const [currentSize, setSize] = useState(`${props.sizes[0].names}`);
+
+  return (
+    <article className={styles.product}>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          alt={props.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
+      </div>
+      <div>
+        <header>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>Price: {props.basePrice}$</span>
+        </header>
+        <form>
+          <div className={styles.sizes}>
+            <h3 className={styles.optionLabel}>Sizes</h3>
+            <ul className={styles.choices}>
+              {props.sizes.map(size => <li><button value={size.name} type="button" className={clsx(size.name === currentSize && styles.active)}>{size.name}</button></li>)}
+
+            </ul>
+          </div>
+          <div className={styles.colors}>
+            <h3 className={styles.optionLabel}>Colors</h3>
+            <ul className={styles.choices}>
+              {props.colors.map(color => <li><button type="button" className={clsx(styles[`color${color[0].toUpperCase()}${color.substr(1).toLowerCase()}`], color === currentColor && styles.active)}></button></li>)}
+
+            </ul>
+          </div>
+          <Button className={styles.button}>
+            <span className="fa fa-shopping-cart" />
+          </Button>
+        </form>
+      </div>
+    </article >
+  )
+};
+
+export default Product;
